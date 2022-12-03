84cc59b1dc53a4298f3afddba4cbb9844f35d2a9
amansoni <amansoni2041999@gmail.com>
2022-12-03 23:58:50 +0530

    welcome to the LogicalProgramming

---
+++ LogicalProgramming222/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalProgramming222
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+             Console.WriteLine("Enter the number check perfect or not : ");
+            int num =Convert.ToInt32(Console.ReadLine());   
+            int temp = 0;
+            for (int i = 1; i < num; i++)
+            {
+                if (num%i==0)
+                {
+                    temp=temp+i;
+                }
+            }
+            if (temp == num)
+            {
+                Console.WriteLine($"{num} is a perfect number");
+
+            }
+            else
+            {
+                Console.WriteLine($"{num} is not a perfect number");
+            }
+            Console.ReadLine();  Console.WriteLine("Enter the number check perfect or not : ");
+           
+        }
+    }
+}
